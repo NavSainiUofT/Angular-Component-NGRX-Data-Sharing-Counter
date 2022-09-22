8fd5d71a2bda8aab7c63a08c498fe011d9a8203e
NavSainiUofT <40246928+NavSainiUofT@users.noreply.github.com>
2022-09-22 16:00:24 -0400

    Add files via upload

---
+++ src/app/counter/state/counter.reducer.ts
@@ -0,0 +1,36 @@
+import { createReducer, on } from "@ngrx/store";
+import { customIncrement, decrement, increment, reset } from "./counter.actions";
+import { initialState } from "./counter.state";
+
+const _counterReducer = createReducer(initialState, 
+    on(increment, (state => {
+    return {
+        ...state,
+        counter: state.counter+1,
+    };
+        }
+        )
+        ),
+    on(decrement, (state) => {
+    return {
+        ...state,
+        counter: state.counter-1,
+    };
+}),
+on(reset, state => {
+    return {
+        ...state,
+        counter: 0,
+    };
+}),
+on(customIncrement, (state, action) => {
+    console.log(action);
+    return {
+        ...state,
+        counter: state.counter + action.count,
+    }
+})
+)
+export function counterReducer(state: any, action: any){
+    return _counterReducer(state,action);
+}
---
+++ src/app/counter/state/counter.state.ts
@@ -0,0 +1,8 @@
+export interface CounterState{
+    counter: number;
+
+}
+
+export const initialState: CounterState = {
+    counter: 0
+}
